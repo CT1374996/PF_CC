75eb087b4c54b2efc50f70c2eec565d3df6e0450
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-03-08 09:42:24 +0000

    [update]admin

--- app/controllers/admin/impressions_controller.rb
+++ app/controllers/admin/impressions_controller.rb
@@ -8,6 +8,12 @@ class Admin::ImpressionsController < ApplicationController
     @impression = Impression.find(params[:id])
   end
 
+  def destroy
+    impression = Impression.find(params[:id])
+    impression.destroy
+    redirect_to admin_impressions_path
+  end
+
   private
   def impression_params
     params.require(:impression).permit(:title, :body)
