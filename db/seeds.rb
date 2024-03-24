929ebfa760147f895cafd3bf64d1d46aa4e45ca0
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-03-24 07:51:31 +0000

    [update]seed testdata

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,39 @@ Admin.create!(
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+code = User.find_or_create_by!(email: "cd@vn") do |user|
+  user.name = "code"
+  user.password = "212121"
+  user.like_game = "RPG、アクション等"
+  user.introduction = "よろしくね"
+end
+
+kinakopurin = User.find_or_create_by!(email: "mz@kp") do |user|
+  user.name = "きなこプリン"
+  user.password = "454545"
+  user.like_game = "リズムゲーム全般"
+  user.introduction = "ひらめきプリン～☆"
+end
+
+stars = User.find_or_create_by!(email: "vio@hzd") do |user|
+  user.name = "stars"
+  user.password = "password"
+  user.like_game = "ホラー"
+  user.introduction = "泣けるぜ"
+end
+
+Impression.find_or_create_by!(title: "ゲームの感想") do |impression|
+  impression.body = "先日クリアしたゲーム『魂王』の感想を書きます。(以下省略)"
+  impression.user = code
+end
+
+Impression.find_or_create_by!(title: "シナリオ考察") do |impression|
+  impression.body = "七音偶像というゲームについての考察です。(以下省略)"
+  impression.user = kinakopurin
+end
+
+Impression.find_or_create_by!(title: "思い出話") do |impression|
+  impression.body = "子供の頃に遊んだ思い出のゲーム『たぬきまち☆パニック』について語ります。(以下省略)"
+  impression.user = stars
+end
