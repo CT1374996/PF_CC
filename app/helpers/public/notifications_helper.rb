5d1bb62c888895720cf3456e0504d904a4d069b2
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-08-27 15:47:15 +0000

    [update]notification

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -1,2 +1,17 @@
 module Public::NotificationsHelper
+  def notification_form(notification)
+    @visitor = notification.visitor
+    @comment = nil
+    @visitor_comment = notification.comment_id
+    case notification.action
+    when "follow"
+      tag.a(notification.visitor.name, href: user_path(@visitor)) + "さんがあなたをフォローしました"
+    when "favorite"
+      tag.a(notification.visitor.name, href: user_path(@visitor)) + "さんが" + tag.a("あなたの投稿", href: impression_path(notification.impression_id)) + "にいいねしました"
+    when "comment"
+      @comment = Comment.find_by(id: @visitor_comment)
+      @impression_title = @comment.impression.title
+      tag.a(@visitor.name, href: user_path(@visitor)) + "さんが" + tag.a("#{@impression_title}", href: impression_path(notification.impression_id)) + "にコメントしました"
+    end
+  end
 end
